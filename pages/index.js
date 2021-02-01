efa030a1603c547eafd511e0f9e6b3ef2b7dd134
WanjinYoo <dksladjsla@gmail.com>
2021-01-31 18:27:40 -0800

    create basic UI component

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,116 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
 
-export default function Home() {
+const useStyles = makeStyles({
+  table: {
+    minWidth: 1000,
+    height: '100%'
+  },
+  chatSection: {
+    width: '100%',
+    height: '100%'
+  },
+  headBG: {
+      backgroundColor: '#e0e0e0'
+  },
+  borderRight500: {
+      borderRight: '1px solid #e0e0e0'
+  },
+  messageArea: {
+    height: '80vh',
+    overflowY: 'auto'
+  }
+});
+
+const Chat = () => {
+  const classes = useStyles();
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <React.Fragment>
+       
+        <Grid container component={Paper} className={classes.chatSection}>
+            <Grid item={true} xs={3} className={classes.borderRight500}>
+                <List>
+                    <ListItem button key="RemySharp">
+                        <ListItemIcon>
+                        <Avatar alt="Wanjin Yoo" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                        </ListItemIcon>
+                        <ListItemText primary="Wanjin Yoo"></ListItemText>
+                    </ListItem>
+                </List>
+                <Divider />
+                <Grid item={true} xs={12} style={{padding: '10px'}}>
+                    <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />
+                </Grid>
+                <Divider />
+                <List>
+                    <ListItem button key="RemySharp">
+                        <ListItemIcon>
+                            <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                        </ListItemIcon>
+                        <ListItemText primary="Remy Sharp">Remy Sharp</ListItemText>
+                        <ListItemText secondary="online" align="right"></ListItemText>
+                    </ListItem>
+                </List>
+            </Grid>
+            <Grid item={true} xs={9}>
+                <List className={classes.messageArea}>
+                    <ListItem key="1">
+                        <Grid container>
+                            <Grid item={true} xs={12}>
+                                <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
+                            </Grid>
+                            <Grid item={true} xs={12}>
+                                <ListItemText align="right" secondary="09:30"></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <ListItem key="2">
+                        <Grid container>
+                            <Grid item={true} xs={12}>
+                                <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
+                            </Grid>
+                            <Grid item={true} xs={12}>
+                                <ListItemText align="left" secondary="09:31"></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <ListItem key="3">
+                        <Grid container>
+                            <Grid item={true} xs={12}>
+                                <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
+                            </Grid>
+                            <Grid item={true} xs={12}>
+                                <ListItemText align="right" secondary="10:30"></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                </List>
+                <Divider />
+                <Grid container style={{padding: '20px'}}>
+                    <Grid item={true} xs={11}>
+                        <TextField id="outlined-basic-email" label="Type Something" fullWidth />
+                    </Grid>
+                    <Grid item={true} xs={1} align="right">
+                        <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+        </React.Fragment>
+  );
 }
+
+export default Chat;
